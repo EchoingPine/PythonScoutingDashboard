8a36674d3038c2b8e2810532c76baab66ee93cdd
Shrimp44578 <smchimu@gmail.com>
2025-12-28 15:36:09 -0500

    Base Dashboard

---
+++ db_calc.py
@@ -0,0 +1,133 @@
+import gspread
+import pandas as pd
+import sqlite3
+import plotly.graph_objects as go
+from google.oauth2.service_account import Credentials
+
+endgame_scores = {
+    "Deep Climb": 12,
+    "Shallow Climb": 6,
+    "Parked": 2,
+    "Nothing": 0
+}
+
+leave_scores = {
+    "TRUE": 3,
+    "FALSE": 0
+}
+
+auto_scores = {
+    "Auto L4": 7,
+    "Auto L3": 6,
+    "Auto L2": 4,
+    "Auto L1": 3,
+    "Auto NET": 4,
+}
+
+teleop_scores = {
+    "L4": 5,
+    "L3": 4,
+    "L2": 3,
+    "L1": 2,
+    "NET": 4,
+    "PROCESSOR": 2,
+}
+
+def write_to_db(dataframe, table_name):
+    conn = sqlite3.connect("Scouting_Data.db")
+    cursor = conn.cursor()
+    dataframe.to_sql(table_name, conn, if_exists="replace", index=False)
+    conn.close()
+
+def perform_calculations():
+    apis = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+    creds = Credentials.from_service_account_file(
+        "data_reader_account.json",
+        scopes=apis
+    )
+
+    gc = gspread.authorize(creds)
+    spreadsheet = gc.open("Copy of NEW AMAZONG 2025 SCOUTING DASHBOARD!!!! (dcmp data)")
+    worksheet = spreadsheet.worksheet("Data Entry")
+    data = worksheet.get_all_records()
+    df = pd.DataFrame(data)
+
+    # Calculate scores
+    df['Auto Score'] = pd.DataFrame(
+        {col: df[col].fillna(0) * weight for col, weight in auto_scores.items()}
+    ).sum(axis=1)
+    df['Auto Score'] += df['Leave (Did the robot move)?'].map(leave_scores).fillna(0)
+
+    df['Teleop Score'] = pd.DataFrame(
+        {col: df[col].fillna(0) * weight for col, weight in teleop_scores.items()}
+    ).sum(axis=1)
+
+    df['Endgame Score'] = df['Endgame'].map(endgame_scores).fillna(0)
+    df['Total Score'] = df['Auto Score'] + df['Teleop Score'] + df['Endgame Score']
+
+    df = df.sort_values(['Team Number', 'Match Number'])
+
+    df['Team Match Number'] = df.groupby('Team Number').cumcount() + 1
+
+    calc_df = pd.DataFrame()
+    calc_df['Team Number'] = df['Team Number'].unique()
+
+    team_counts = (
+        df.groupby('Team Number')
+        .size()
+        .reset_index(name='Matches Played')
+    )
+
+
+    auto_cols = ['Auto L1', 'Auto L2', 'Auto L3', 'Auto L4']
+    auto_avg = (
+        df.groupby('Team Number')[auto_cols]
+        .mean()
+        .sum(axis=1)
+        .reset_index(name='Auto Coral AVG')
+    )
+
+    teleop_cols = ['L1', 'L2', 'L3', 'L4']
+    teleop_avg = (
+        df.groupby('Team Number')[teleop_cols]
+        .mean()
+        .sum(axis=1)
+        .reset_index(name='Teleop Coral AVG')
+    )
+
+
+
+    calc_df = df.groupby('Team Number', as_index=False).agg(
+        **{
+            'Auto Score AVG': ('Auto Score', 'mean'),
+            'Teleop Net Algae AVG': ('NET', 'mean'),
+            'Teleop Processor Algae AVG': ('PROCESSOR', 'mean'),
+            'Teleop Score AVG': ('Teleop Score', 'mean'),
+            'Climb Score AVG': ('Endgame Score', 'mean'),
+            'Total Score AVG': ('Total Score', 'mean')
+        }
+    )
+
+    calc_df = (
+        calc_df
+        .merge(auto_avg, on='Team Number')
+        .merge(team_counts, on='Team Number')
+        .merge(teleop_avg, on='Team Number')
+    )
+
+    # Reorder columns
+    calc_df = calc_df[
+        ['Team Number', 'Auto Coral AVG', 'Auto Score AVG', 'Teleop Net Algae AVG', 'Teleop Processor Algae AVG', 'Teleop Coral AVG', 'Teleop Score AVG', 'Climb Score AVG', 'Total Score AVG']
+    ]
+
+    calc_df = calc_df.round(2)
+
+
+    write_to_db(calc_df, "Calcs")
+
+    write_to_db(df, "Scouting_Data")
+
+perform_calculations()
---
+++ main.py
@@ -0,0 +1,188 @@
+import gspread
+import pandas as pd
+import sqlite3
+import streamlit as st
+import plotly.graph_objects as go
+import matplotlib.cm as cm
+import matplotlib.colors as mc
+import matplotlib
+from google.oauth2.service_account import Credentials
+
+
+conn = sqlite3.connect("Scouting_Data.db")
+cursor = conn.cursor()
+
+st.set_page_config(layout="wide")
+
+def retrieve_data(data_type, team_number, match_number=None):
+    if match_number is None:
+        query = f'SELECT "{data_type}" FROM Scouting_Data WHERE "Team Number" = ?'
+        params = (team_number,)
+    else:
+        query = f'SELECT "{data_type}" FROM Scouting_Data WHERE "Team Number" = ? AND "Match Number" = ?'
+        params = (team_number, match_number)
+    cursor.execute(query, params)
+    return cursor.fetchall()
+
+
+def plot_team_scores(team_number, show_table=False):
+    team_data = pd.read_sql(
+        f"SELECT * FROM Scouting_Data WHERE `Team Number` = {team_number} ORDER BY `Team Match Number` ASC",
+        conn
+    )
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(x=team_data['Team Match Number'], y=team_data['Total Score'], mode='lines+markers', name='Total Score'))
+    fig.add_trace(
+        go.Scatter(x=team_data['Team Match Number'], y=team_data['Auto Score'], mode='lines+markers', name='Auto Score'))
+    fig.add_trace(
+        go.Scatter(x=team_data['Team Match Number'], y=team_data['Teleop Score'], mode='lines+markers', name='Teleop Score'))
+    fig.add_trace(
+        go.Scatter(x=team_data['Team Match Number'], y=team_data['Endgame Score'], mode='lines+markers', name='Endgame Score'))
+
+    fig.update_layout(
+        title=f"Team {team_number} Score Trend",
+        xaxis_title="Match Number",
+        yaxis_title="Score",
+        yaxis=dict(range=[0, 125]),
+    )
+
+    st.plotly_chart(fig, width="stretch")
+    if show_table:
+        st.dataframe(team_data)
+
+dataType = st.sidebar.selectbox("View", ["Single Team", "Compare", "Averages", "Match Reference", "Bubble Chart"])
+AutoCols = ['Auto Coral AVG', 'Auto Score AVG']
+
+averageMatchScore = 0
+averageScore = 0
+
+if dataType.lower() == "single team":
+    try:
+        teamNumber = int(st.sidebar.text_input("Team Number", "1100"))
+    except ValueError:
+        st.error("Please enter a valid integer team number.")
+        st.stop()
+
+    df = pd.read_sql("SELECT * FROM 'Scouting_Data'", conn)
+    team_data = df[df['Team Number'] == teamNumber]
+
+    if not team_data.empty:
+        plot_team_scores(teamNumber, True)
+    else:
+        st.warning(f"No Data for Team {teamNumber}")
+
+
+elif dataType.lower() == "compare":
+    teamNumbers = []
+    for i in range(1, 7):
+        input_value = st.sidebar.text_input(f"Team {i}", "")
+        if input_value.strip():
+            try:
+                teamNumber = int(input_value)
+                teamNumbers.append(teamNumber)
+            except ValueError:
+                st.error(f"Please enter a valid number for Team {i}.")
+                st.stop()
+
+    df = pd.read_sql("SELECT * FROM 'Scouting_Data'", conn)
+
+    for i in range(0, len(teamNumbers), 3):
+        columns = st.columns(3)
+        for j, teamNumber in enumerate(teamNumbers[i:i + 3]):
+            team_data = df[df['Team Number'] == teamNumber]
+            if not team_data.empty:
+                with columns[j]:
+                    plot_team_scores(teamNumber)
+            else:
+                with columns[j]:
+                    st.warning(f"No Data for Team {teamNumber}")
+
+elif dataType.lower() == "averages":
+    df = pd.read_sql("SELECT * FROM Calcs", conn)
+
+    AutoColors = ["#010014", "#252525"]
+    AutoCmap = mc.LinearSegmentedColormap.from_list("BlueGray", AutoColors)
+    TeleopColors = ["#301500", "#252525"]
+    TeleopCmap = mc.LinearSegmentedColormap.from_list("OrangeGray", TeleopColors)
+    EndgameColors = ["#302d00", "#252525"]
+    EndgameCmap = mc.LinearSegmentedColormap.from_list("YellowGray", EndgameColors)
+    TotalColors = ["#003003", "#252525"]
+    TotalCmap = mc.LinearSegmentedColormap.from_list("GreenGray", TotalColors)
+
+    AutoCols = ['Auto Coral AVG', 'Auto Score AVG']
+    TeleopCols = ['Teleop Net Algae AVG', 'Teleop Processor Algae AVG', 'Teleop Score AVG']
+    EndgameCols = ['Climb Score AVG']
+    TotalCols = ['Total Score AVG']
+    ScoringCols = ['Auto Coral AVG', 'Auto Score AVG', 'Teleop Net Algae AVG', 'Teleop Processor Algae AVG' ,'Teleop Score AVG', 'Climb Score AVG', 'Total Score AVG']
+
+    df = df.style.format("{:.2f}", subset=ScoringCols).background_gradient(cmap=AutoCmap, subset=AutoCols, axis=0).background_gradient(cmap=TeleopCmap, subset=TeleopCols, axis=0).background_gradient(cmap=EndgameCmap, subset=EndgameCols, axis=0).background_gradient(cmap=TotalCmap, subset=TotalCols, axis=0)
+    st.dataframe(df)
+
+elif dataType.lower() == "match reference":
+    try:
+        matchNumber = int(st.sidebar.text_input("Match Number", "1"))
+    except ValueError:
+        st.error("Please enter a valid integer team number.")
+        st.stop()
+
+    teams_df = pd.read_sql(
+        'SELECT DISTINCT "Team Number" FROM "Scouting_Data" WHERE "Match Number" = ?',
+        conn,
+        params=(matchNumber,)
+    )
+
+    avg_scores_df = pd.read_sql(
+        'SELECT "Team Number", "Auto Score AVG", "Teleop Score AVG", "Climb Score AVG", "Total Score AVG" FROM "Calcs"',
+        conn
+    )
+
+    result_df = teams_df.merge(avg_scores_df, on="Team Number", how="left")
+
+    st.dataframe(result_df)
+
+elif dataType.lower() == "bubble chart":
+    df = pd.read_sql('SELECT * FROM "Calcs"', conn)
+
+    xAxis = st.sidebar.selectbox("X-Axis", ['Select X-Axis'] + df.columns.tolist())
+    yAxis = st.sidebar.selectbox("Y-Axis", ['Select Y-Axis'] + df.columns.tolist())
+
+    # Create figure
+    fig = go.Figure()
+
+    if xAxis == 'Select X-Axis' or yAxis == 'Select Y-Axis':
+        st.info("Select Axes to Continue")
+
+    else:
+        fig.add_trace(
+            go.Scatter(
+                x=df[xAxis],
+                y=df[yAxis],
+                mode='markers+text',
+                marker=dict(
+                    size=12,
+                ),
+                text=df['Team Number'],
+                textposition='bottom center',
+                textfont=dict(
+                    size=14,
+                    color='white'
+                ),
+                hovertemplate=(
+                        "Team: %{text}<br>"
+                        f"{xAxis}: " + "%{x}<br>"
+                                       f"{yAxis}: " + "%{y}<br>"
+                )
+            )
+        )
+
+        fig.update_layout(
+            title=f"{yAxis} vs {xAxis} Scatter Plot",
+            xaxis_title=xAxis,
+            yaxis_title=yAxis,
+        )
+
+        st.plotly_chart(fig)
+
+
